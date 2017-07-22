f0cbe57a496c225228f1b47c3199b2542fe9b615
Souvik Ghosh <emailtosvk@gmail.com>
2017-07-22 17:24:05 +0530

    initial commit

---
+++ app/views/templates/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>My Cms</title>
+	<link rel="stylesheet" href="../css/app.css" >
+</head>
+<body>
+	<div class="wrapper">
+		<h1>Tiny CMS</h1>
